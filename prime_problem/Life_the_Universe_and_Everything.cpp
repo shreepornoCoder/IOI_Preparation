5d08c0225b46ead0cf1d5241f28f1ff401142a10
shreepornoCoder <sontos1979@gmail.com>
2025-04-17 23:52:03 +0600

    File Added

---
+++ prime_problem/Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,14 @@
+// First problem on SPOJ
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for (int i = 0;;i++){
+        int val; cin>>val;
+
+        if (val != 42) cout<<val<<"\n";
+        else break;
+    }
+    return 0;
+}
